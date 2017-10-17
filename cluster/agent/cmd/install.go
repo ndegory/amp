047aa1a3721a573477035a1565a397ed43a8f725
Nicolas Degory <ndegory@axway.com>
2017-10-17 10:27:20 -0700

    jwilder/nginx-proxy

--- cluster/agent/cmd/install.go
+++ cluster/agent/cmd/install.go
@@ -516,6 +516,7 @@ func CreateConfig(name string, data []byte) error {
 var ampConfigs = map[string]string{
 	"prometheus_alerts_rules": "prometheus_alerts.rules",
 	"api_proxy_cfg":           "api_proxy.cfg",
+	"nginx_template":          "nginx.tmpl",
 }
 
 // This is the default configs path
